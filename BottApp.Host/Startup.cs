62ea69969277752371472734ab6e81c2af1659e9
Alexander <vsany@meta.ua>
2022-10-08 01:53:12 +0300

    [+] added cors request ( post get delete ). Added Database registration in IServiceCollection

--- BottApp.Host/Startup.cs
+++ BottApp.Host/Startup.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BottApp.Database;
+using BottApp.Host.Configs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -16,20 +19,71 @@ namespace BottApp.Host
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        private IWebHostEnvironment CurrentEnvironment{ get; set; }
+        
+        private ILogger _logger;
+        private ILoggerFactory _loggerFactory;
+
+        
+        public Startup(IConfiguration configuration, IWebHostEnvironment hostEnvironment, ILoggerFactory loggerFactory)
         {
             Configuration = configuration;
+            CurrentEnvironment = hostEnvironment;
+            _logger = loggerFactory.CreateLogger<Startup>();
+            _loggerFactory = loggerFactory;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors
+            (
+                options =>
+                {
+                    options.AddPolicy
+                    (
+                        "CorsPolicy",
+                        policy =>
+                            policy.WithOrigins(Configuration.GetSection("AllowedHosts").Value)
+                                .WithMethods("POST", "GET", "DELETE", "OPTIONS")
+                                .WithHeaders("*")
+                    );
+                    options.AddPolicy
+                    (
+                        "apiDocumentation",
+                        policy =>
+                            policy.WithOrigins("*")
+                                .WithMethods("POST", "GET", "DELETE")
+                                .WithHeaders("*")
+                    );
+                }
+            );
+            
+            ConfigureCoreServices(services, CurrentEnvironment);
+
+            
             services.AddSwaggerGen(
                 c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "BottApp.Host", Version = "v1"}); }
             );
+        }
+
+        private void ConfigureCoreServices(IServiceCollection services, IWebHostEnvironment env)
+        {
+            // services.Configure<BotConfig>(Configuration.GetSection("Token"));
+
+            Type typeOfContent = typeof(Startup);
+            
+            services.AddDbContext<PostgreSqlContext>(
+                opt => opt.UseNpgsql(
+                    Configuration.GetConnectionString("PostgreSqlConnection"),
+                    b => b.MigrationsAssembly(typeOfContent.Assembly.GetName().Name)
+                )
+            );
+
+            services.AddScoped<IDatabaseContainer, DatabaseContainer>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
