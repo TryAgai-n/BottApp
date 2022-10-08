be9f3859f01a395f8ede44e85f1b90dd2670464f
Alexander <vsany@meta.ua>
2022-10-08 05:08:21 +0300

    [*] moved Slava's files to another package

---
+++ BottApp.Host/Controllers/Client/DownloaManager.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+
+namespace BottApp.Host
+{
+    public class DownloaManager
+    {
+        static string FileID { get; set; }
+        async static public void DownloadDocument(ITelegramBotClient botClient, Message message, MessageType type)
+        {
+
+
+            try
+            {
+                if (type == MessageType.Photo)
+                {
+                    var _field = message.Photo[message.Photo.Length - 1].FileId;
+                }
+                if (type == MessageType.Document)
+                {
+                    var _field = message.Document.FileId;
+                }
+                if (type == MessageType.Voice)
+                {
+                    var _field = message.Voice.FileId;
+                }
+                if (type == MessageType.Video)
+                {
+                    var _field = message.Video.FileId;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{type} | Error downloading: " + ex.Message);
+                return;
+            }
+
+            finally
+            {
+
+                if (type == MessageType.Photo)
+                {
+                    FileID = message.Photo[message.Photo.Length - 1].FileId;
+                }
+                if (type == MessageType.Document)
+                {
+                    FileID = message.Document.FileId;
+                }
+                if (type == MessageType.Voice)
+                {
+                    FileID = message.Voice.FileId;
+                }
+                if (type == MessageType.Video)
+                {
+                    FileID = message.Video.FileId;
+                }
+            }
+
+            var fileInfo = await botClient.GetFileAsync(FileID);
+            var filePath = fileInfo.FilePath;
+            var extension = Path.GetExtension(filePath);
+
+            
+            var rootPath = Directory.GetCurrentDirectory() + "/DATA/";
+            var folderName = type;
+            var newPath = Path.Combine(rootPath, message.Chat.FirstName, folderName.ToString(), extension);
+
+            if (!Directory.Exists(newPath))
+            {
+                Directory.CreateDirectory(newPath);
+            }
+
+            string destinationFilePath = newPath + $"/{message.Chat.FirstName}__{Guid.NewGuid().ToString("N")}__{message.Chat.Id}__{extension}";
+
+            await using FileStream fileStream = System.IO.File.OpenWrite(destinationFilePath);
+            await botClient.DownloadFileAsync(filePath, fileStream);
+            fileStream.Close();
+
+            Console.WriteLine(
+                $"Документ {type} получен от пользователя {message.Chat.FirstName}" +
+                $" ID {message.Chat.Id}, " +
+                $" размер документа {fileInfo.FileSize} байт." +
+                $" \nFULLPATH {destinationFilePath}");
+
+            await botClient.SendTextMessageAsync(message.Chat.Id, "Спасибо! Ваш документ загружен в базу данных.");
+
+            return;
+
+        }
+
+    }
+}
---
+++ BottApp.Host/Controllers/Client/Stiker.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace BottApp
+{
+    public class Stiker
+    {
+        public string Description { get; set; }
+        public string Stiker_ID { get; set; }
+
+        public Stiker(string _description, string _stiker_ID)
+        {
+            Description = _description;
+            Stiker_ID = _stiker_ID;
+        }
+    }
+}
