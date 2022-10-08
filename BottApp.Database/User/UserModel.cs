7d4a4f9657ba4207378746a171f71ee7191b10bc
Alexander <vsany@meta.ua>
2022-10-08 05:09:09 +0300

    temp user model

--- BottApp.Database/User/UserModel.cs
+++ BottApp.Database/User/UserModel.cs
@@ -7,7 +7,16 @@ public class UserModel : AbstractModel, IUser
     public long Id { get; set; }
 
     public string FirstName { get; set; }
+
     public string Phone { get; set; }
 
     public bool IsSendContact { get; set; }
+
+    public UserModel(long id, string firstName, string phone, bool isSendContact)
+    {
+        Id = id;
+        FirstName = firstName;
+        Phone = phone;
+        IsSendContact = isSendContact;
+    }
 }
