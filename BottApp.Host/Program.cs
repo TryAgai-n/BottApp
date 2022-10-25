4d812981e386bf2960e825ef9cb5ab90954b4d44
User <gold.like.simple@gmail.com>
2022-10-26 00:16:30 +1000

    base setup of BottApp.Host project

--- BottApp.Host/Program.cs
+++ BottApp.Host/Program.cs
@@ -1,42 +1,156 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using BottApp.Data;
-using BottApp.Host;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace BottApp.Host
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<PostgreSqlContext>(
-    o=> o.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
-builder.Services.AddScoped<IDatabaseContainer, DatabaseContainer>();
+            builder.Services.AddDbContext<PostgreSqlContext>(
+                o=> o.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
+            builder.Services.AddScoped<IDatabaseContainer, DatabaseContainer>();
 
 //Start Bot
-var token = builder.Configuration.GetSection("Token").Value;
-var bot = new BotInit();
+            var token = builder.Configuration.GetSection("Token").Value;
+            var bot = new BotInit();
 
-bot.initReceiver(token);
+            bot.initReceiver(token);
+            
+            
+            
+            var webHost = BuildWebHost(args);
+            var commandLineApplication = new CommandLineApplication(false);
+
+            var doMigrate = commandLineApplication.Option(
+                "--ef-migrate",
+                "Apply entity framework migrations and exit",
+                CommandOptionType.NoValue
+            );
+            var verifyMigrate = commandLineApplication.Option(
+                "--ef-migrate-check",
+                "Check the status of entity framework migrations",
+                CommandOptionType.NoValue
+            );
+            var run = commandLineApplication.Option(
+                "--run",
+                "Run api Server",
+                CommandOptionType.NoValue
+            );
+
+            commandLineApplication.HelpOption("-? | -h | --help");
+            commandLineApplication.OnExecute(
+                () =>
+                {
+                    ExecuteApp(webHost, doMigrate, verifyMigrate, run);
+                    return 0;
+                }
+            );
+            commandLineApplication.Execute(args);
+        }
 
 
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .Build();
+
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls(config.GetSection("Url").Value)
+                .Build();
+        }
 
 
+        private static IWebHost ExecuteApp(
+            IWebHost webHost,
+            CommandOption doMigrate,
+            CommandOption verifyMigrate,
+            CommandOption run
+        )
+        {
+            if (run.HasValue())
+            {
+                InitWebServices(webHost).GetAwaiter().GetResult();
+                return webHost;
+            }
 
-var app = builder.Build();
+            if (verifyMigrate.HasValue() && doMigrate.HasValue())
+            {
+                Console.WriteLine("ef-migrate and ef-migrate-check are mutually exclusive, select one, and try again");
+                Environment.Exit(2);
+            }
+
+            var serviceScopeFactory = (IServiceScopeFactory) webHost.Services.GetService(typeof(IServiceScopeFactory));
+            using (var scope = serviceScopeFactory.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                using (var context = services.GetRequiredService<PostgreSqlContext>())
+                {
+                    if (verifyMigrate.HasValue())
+                    {
+                        Console.WriteLine("Validating status of Entity Framework migrations");
+                        _verifyMigrate(context);
+                        Environment.Exit(3);
+                    }
+
+                    if (doMigrate.HasValue())
+                    {
+                        Console.WriteLine("Applyting Entity Framework migrations");
+                        context.Database.Migrate();
+                        Console.WriteLine("All done, closing app");
+                        Environment.Exit(0);
+                    }
+                }
+            }
+
+            InitWebServices(webHost).GetAwaiter().GetResult();
+            return webHost;
+        }
+
+        
+        private static void _verifyMigrate(PostgreSqlContext context)
+        {
+            var pendingMigrations = context.Database.GetPendingMigrations();
+            var migrations = pendingMigrations as IList<string> ?? pendingMigrations.ToList();
+            if (!migrations.Any())
+            {
+                Console.WriteLine("No pending migratons");
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Pending migratons {0}", migrations.Count());
+            foreach (var migration in migrations)
+            {
+                Console.WriteLine($"\t{migration}");
+            }
+        }
 
 
+        private static async Task<int> InitWebServices(IWebHost webHost)
+        {
+            await Task.WhenAll(webHost.RunAsync());
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
+            Console.Read();
+            await Task.WhenAll(webHost.StopAsync());
+            Environment.Exit(0);
+            return 0;
+        }
+    }
 }
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
---
+++ BottApp.Host/Startup.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BottApp.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace BottApp.Host
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        private IWebHostEnvironment CurrentEnvironment{ get; set; }
+        
+        private ILogger _logger;
+        private ILoggerFactory _loggerFactory;
+
+        
+        public Startup(IConfiguration configuration, IWebHostEnvironment hostEnvironment, ILoggerFactory loggerFactory)
+        {
+            Configuration = configuration;
+            CurrentEnvironment = hostEnvironment;
+            _logger = loggerFactory.CreateLogger<Startup>();
+            _loggerFactory = loggerFactory;
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddCors
+            (
+                options =>
+                {
+                    options.AddPolicy
+                    (
+                        "CorsPolicy",
+                        policy =>
+                            policy.WithOrigins(Configuration.GetSection("AllowedHosts").Value)
+                                .WithMethods("POST", "GET", "DELETE", "OPTIONS")
+                                .WithHeaders("*")
+                    );
+                    options.AddPolicy
+                    (
+                        "apiDocumentation",
+                        policy =>
+                            policy.WithOrigins("*")
+                                .WithMethods("POST", "GET", "DELETE")
+                                .WithHeaders("*")
+                    );
+                }
+            );
+            
+            ConfigureCoreServices(services, CurrentEnvironment);
+            
+        }
+
+        private void ConfigureCoreServices(IServiceCollection services, IWebHostEnvironment env)
+        {
+
+            Type typeOfContent = typeof(Startup);
+            
+            services.AddDbContext<PostgreSqlContext>(
+                opt => opt.UseNpgsql(
+                    Configuration.GetConnectionString("PostgreSqlConnection"),
+                    b => b.MigrationsAssembly(typeOfContent.Assembly.GetName().Name)
+                )
+            );
+
+            services.AddScoped<IDatabaseContainer, DatabaseContainer>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
+
+
+// Start Bot
+// var token = builder.Configuration.GetSection("Token").Value;
+// var bot = new BotInit();
+//
+// bot.initReceiver(token);
+//
+// var app = builder.Build();
