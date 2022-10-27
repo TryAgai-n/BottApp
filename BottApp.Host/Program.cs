583ca94d34ce1eea1afc289fbc8db77f8bc6525b
Alexander <vsany@meta.ua>
2022-10-27 03:02:41 +0300

    Program -> commented some life circle

--- BottApp.Host/Program.cs
+++ BottApp.Host/Program.cs
@@ -17,50 +17,35 @@ namespace BottApp.Host
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.AddControllersWithViews();
-
-            builder.Services.AddDbContext<PostgreSqlContext>(
-                o=> o.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
-            builder.Services.AddScoped<IDatabaseContainer, DatabaseContainer>();
-
-//Start Bot
-            var token = builder.Configuration.GetSection("Token").Value;
-            var bot = new BotInit();
-
-            bot.initReceiver(token);
-            
-            
-            
             var webHost = BuildWebHost(args);
-            var commandLineApplication = new CommandLineApplication(false);
-
-            var doMigrate = commandLineApplication.Option(
-                "--ef-migrate",
-                "Apply entity framework migrations and exit",
-                CommandOptionType.NoValue
-            );
-            var verifyMigrate = commandLineApplication.Option(
-                "--ef-migrate-check",
-                "Check the status of entity framework migrations",
-                CommandOptionType.NoValue
-            );
-            var run = commandLineApplication.Option(
-                "--run",
-                "Run api Server",
-                CommandOptionType.NoValue
-            );
-
-            commandLineApplication.HelpOption("-? | -h | --help");
-            commandLineApplication.OnExecute(
-                () =>
-                {
-                    ExecuteApp(webHost, doMigrate, verifyMigrate, run);
-                    return 0;
-                }
-            );
-            commandLineApplication.Execute(args);
+            
+            // var commandLineApplication = new CommandLineApplication(false);
+            //
+            // var doMigrate = commandLineApplication.Option(
+            //     "--ef-migrate",
+            //     "Apply entity framework migrations and exit",
+            //     CommandOptionType.NoValue
+            // );
+            // var verifyMigrate = commandLineApplication.Option(
+            //     "--ef-migrate-check",
+            //     "Check the status of entity framework migrations",
+            //     CommandOptionType.NoValue
+            // );
+            // var run = commandLineApplication.Option(
+            //     "--run",
+            //     "Run api Server",
+            //     CommandOptionType.NoValue
+            // );
+            //
+            // commandLineApplication.HelpOption("-? | -h | --help");
+            // commandLineApplication.OnExecute(
+            //     () =>
+            //     {
+            //         ExecuteApp(webHost, doMigrate, verifyMigrate, run);
+            //         return 0;
+            //     }
+            // );
+            // commandLineApplication.Execute(args);
         }
 
 
@@ -78,79 +63,79 @@ namespace BottApp.Host
         }
 
 
-        private static IWebHost ExecuteApp(
-            IWebHost webHost,
-            CommandOption doMigrate,
-            CommandOption verifyMigrate,
-            CommandOption run
-        )
-        {
-            if (run.HasValue())
-            {
-                InitWebServices(webHost).GetAwaiter().GetResult();
-                return webHost;
-            }
+        // private static IWebHost ExecuteApp(
+        //     IWebHost webHost,
+        //     CommandOption doMigrate,
+        //     CommandOption verifyMigrate,
+        //     CommandOption run
+        // )
+        // {
+        //     if (run.HasValue())
+        //     {
+        //         InitWebServices(webHost).GetAwaiter().GetResult();
+        //         return webHost;
+        //     }
+        //
+        //     if (verifyMigrate.HasValue() && doMigrate.HasValue())
+        //     {
+        //         Console.WriteLine("ef-migrate and ef-migrate-check are mutually exclusive, select one, and try again");
+        //         Environment.Exit(2);
+        //     }
+        //
+        //     var serviceScopeFactory = (IServiceScopeFactory) webHost.Services.GetService(typeof(IServiceScopeFactory));
+        //     using (var scope = serviceScopeFactory.CreateScope())
+        //     {
+        //         var services = scope.ServiceProvider;
+        //         using (var context = services.GetRequiredService<PostgreSqlContext>())
+        //         {
+        //             if (verifyMigrate.HasValue())
+        //             {
+        //                 Console.WriteLine("Validating status of Entity Framework migrations");
+        //                 _verifyMigrate(context);
+        //                 Environment.Exit(3);
+        //             }
+        //
+        //             if (doMigrate.HasValue())
+        //             {
+        //                 Console.WriteLine("Applyting Entity Framework migrations");
+        //                 context.Database.Migrate();
+        //                 Console.WriteLine("All done, closing app");
+        //                 Environment.Exit(0);
+        //             }
+        //         }
+        //     }
+        //
+        //     InitWebServices(webHost).GetAwaiter().GetResult();
+        //     return webHost;
+        // }
+        //
+        //
+        // private static void _verifyMigrate(PostgreSqlContext context)
+        // {
+        //     var pendingMigrations = context.Database.GetPendingMigrations();
+        //     var migrations = pendingMigrations as IList<string> ?? pendingMigrations.ToList();
+        //     if (!migrations.Any())
+        //     {
+        //         Console.WriteLine("No pending migratons");
+        //         Environment.Exit(0);
+        //     }
+        //
+        //     Console.WriteLine("Pending migratons {0}", migrations.Count());
+        //     foreach (var migration in migrations)
+        //     {
+        //         Console.WriteLine($"\t{migration}");
+        //     }
+        // }
 
-            if (verifyMigrate.HasValue() && doMigrate.HasValue())
-            {
-                Console.WriteLine("ef-migrate and ef-migrate-check are mutually exclusive, select one, and try again");
-                Environment.Exit(2);
-            }
 
-            var serviceScopeFactory = (IServiceScopeFactory) webHost.Services.GetService(typeof(IServiceScopeFactory));
-            using (var scope = serviceScopeFactory.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                using (var context = services.GetRequiredService<PostgreSqlContext>())
-                {
-                    if (verifyMigrate.HasValue())
-                    {
-                        Console.WriteLine("Validating status of Entity Framework migrations");
-                        _verifyMigrate(context);
-                        Environment.Exit(3);
-                    }
-
-                    if (doMigrate.HasValue())
-                    {
-                        Console.WriteLine("Applyting Entity Framework migrations");
-                        context.Database.Migrate();
-                        Console.WriteLine("All done, closing app");
-                        Environment.Exit(0);
-                    }
-                }
-            }
-
-            InitWebServices(webHost).GetAwaiter().GetResult();
-            return webHost;
-        }
-
-        
-        private static void _verifyMigrate(PostgreSqlContext context)
-        {
-            var pendingMigrations = context.Database.GetPendingMigrations();
-            var migrations = pendingMigrations as IList<string> ?? pendingMigrations.ToList();
-            if (!migrations.Any())
-            {
-                Console.WriteLine("No pending migratons");
-                Environment.Exit(0);
-            }
-
-            Console.WriteLine("Pending migratons {0}", migrations.Count());
-            foreach (var migration in migrations)
-            {
-                Console.WriteLine($"\t{migration}");
-            }
-        }
-
-
-        private static async Task<int> InitWebServices(IWebHost webHost)
-        {
-            await Task.WhenAll(webHost.RunAsync());
-
-            Console.Read();
-            await Task.WhenAll(webHost.StopAsync());
-            Environment.Exit(0);
-            return 0;
-        }
+        // private static async Task<int> InitWebServices(IWebHost webHost)
+        // {
+        //     await Task.WhenAll(webHost.RunAsync());
+        //
+        //     Console.Read();
+        //     await Task.WhenAll(webHost.StopAsync());
+        //     Environment.Exit(0);
+        //     return 0;
+        // }
     }
 }
