614d2cf1275c219548ce7793d0c48a51f93609e8
Alexander <vsany@meta.ua>
2022-11-06 17:31:26 +0200

    program

--- BotApp.NewHost/Program.cs
+++ BotApp.NewHost/Program.cs
@@ -3,37 +3,43 @@ using Microsoft.EntityFrameworkCore;
 using Telegram.Bot;
 using Telegram.Bot.Services;
 
-AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices((context, services) =>
+class Program
+{
+    public static async Task Main(string[] args)
     {
-        // Register Bot configuration
-        services.Configure<BotConfiguration>(
-            context.Configuration.GetSection(BotConfiguration.Configuration));
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+        IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) =>
+            {
+                // Register Bot configuration
+                services.Configure<BotConfiguration>(
+                    context.Configuration.GetSection(BotConfiguration.Configuration));
 
 
-        services.AddHttpClient("telegram_bot_client")
-                .AddTypedClient<ITelegramBotClient>((httpClient, sp) =>
-                {
-                    BotConfiguration? botConfig = sp.GetConfiguration<BotConfiguration>();
-                    TelegramBotClientOptions options = new(botConfig.BotToken);
-                    return new TelegramBotClient(options, httpClient);
-                });
-        // Register DB configuration
+                services.AddHttpClient("telegram_bot_client")
+                    .AddTypedClient<ITelegramBotClient>((httpClient, sp) =>
+                    {
+                        BotConfiguration? botConfig = sp.GetConfiguration<BotConfiguration>();
+                        TelegramBotClientOptions options = new(botConfig.BotToken);
+                        return new TelegramBotClient(options, httpClient);
+                    });
+                // Register DB configuration
 
-        services.AddDbContext<PostgreSqlContext>(options =>
-            options.UseNpgsql(context.Configuration.GetConnectionString("PostgreSqlConnection")));
+                services.AddDbContext<PostgreSqlContext>(options =>
+                    options.UseNpgsql(context.Configuration.GetConnectionString("PostgreSqlConnection")));
         
-        services.AddScoped<IDatabaseContainer, DatabaseContainer>();
+                services.AddScoped<IDatabaseContainer, DatabaseContainer>();
        
-        services.AddScoped<UpdateHandler>();
-        services.AddScoped<ReceiverService>();
-        services.AddHostedService<PollingService>();
-    })
-    .Build();
+                services.AddScoped<UpdateHandler>();
+                services.AddScoped<ReceiverService>();
+                services.AddHostedService<PollingService>();
+            })
+            .Build();
 
-await host.RunAsync();
+        await host.RunAsync();
+    }
+}
 
 public class BotConfiguration
 {
