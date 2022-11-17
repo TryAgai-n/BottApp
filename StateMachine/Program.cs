fabb95816b60ba2a4d10d5d3aee86438f02b9d28
Вячеслав Шовкалюк <gold.like.simple@gmail.com>
2022-11-18 09:33:04 +1000

    Class "Program" was uncommented In StateMachine solution

--- StateMachine/Program.cs
+++ StateMachine/Program.cs
@@ -17,45 +17,45 @@ namespace StateMachine
             
             
             
-            //
-            // var bot = new StateMachine<MState, MAction>(MState.Auth);
-            // Console.WriteLine($"Start_State: {bot.State} \n"); // Точка входа - Auth
-            //
-            // bot.Configure(MState.Auth)
-            //     .OnEntry(s => Console.WriteLine($"Entry to {s.Destination} in {s.Source}"))
-            //     .OnExit(s => Console.WriteLine($"Exit in {s.Source}, Destination {s.Destination}"))
-            //     .PermitReentry(MAction.GetAuth)
-            //     .Permit(MAction.GetMainMenu, MState.MainMenu);
-            //
-            // bot.Configure(MState.MainMenu)
-            //     .OnEntry(s => Console.WriteLine($"Entry to {s.Destination} in {s.Source}"))
-            //     .OnExit(s => Console.WriteLine($"Exit in {s.Source}, Destination {s.Destination}"))
-            //     .Permit(MAction.GetVotes, MState.Votes)
-            //     .Permit(MAction.GetHelp, MState.Help);
-            //
-            //
-            // bot.Configure(MState.Votes)
-            //     .OnEntryAsync
-            //     (s => 
-            //         { 
-            //             Console.WriteLine($"Entry ASYNC to {s.Destination} in {s.Source}");
-            //             return Votes.RunVotes(bot, true); 
-            //         }
-            //     )
-            //     
-            //     .OnExit(s => Console.WriteLine($"Exit in {s.Source}, Destination {s.Destination}"))
-            //     .Permit(MAction.GetMainMenu, MState.MainMenu)
-            //     .Permit(MAction.GetVotesDB, MState.OnVotesDB);
-            //
-            //
-            // // string graph = UmlDotGraph.Format(bot.GetInfo());
-            // // Console.WriteLine("GPAPH||||||||||  \n" + graph + "\n||||||||||");
-            //
-            //  bot.Fire(MAction.GetMainMenu);
-            //  Console.WriteLine($"CurrentState: {bot.State}\n");
-            //
-            //  bot.FireAsync(MAction.GetAuth);
-            //  Console.WriteLine($"CurrentState: {bot.State}\n");
+            
+            var bot = new StateMachine<MState, MAction>(MState.Auth);
+            Console.WriteLine($"Start_State: {bot.State} \n"); // Точка входа - Auth
+            
+            bot.Configure(MState.Auth)
+                .OnEntry(s => Console.WriteLine($"Entry to {s.Destination} in {s.Source}"))
+                .OnExit(s => Console.WriteLine($"Exit in {s.Source}, Destination {s.Destination}"))
+                .PermitReentry(MAction.GetAuth)
+                .Permit(MAction.GetMainMenu, MState.MainMenu);
+            
+            bot.Configure(MState.MainMenu)
+                .OnEntry(s => Console.WriteLine($"Entry to {s.Destination} in {s.Source}"))
+                .OnExit(s => Console.WriteLine($"Exit in {s.Source}, Destination {s.Destination}"))
+                .Permit(MAction.GetVotes, MState.Votes)
+                .Permit(MAction.GetHelp, MState.Help);
+            
+            
+            bot.Configure(MState.Votes)
+                .OnEntryAsync
+                (s => 
+                    { 
+                        Console.WriteLine($"Entry ASYNC to {s.Destination} in {s.Source}");
+                        return Votes.RunVotes(bot, true); 
+                    }
+                )
+                
+                .OnExit(s => Console.WriteLine($"Exit in {s.Source}, Destination {s.Destination}"))
+                .Permit(MAction.GetMainMenu, MState.MainMenu)
+                .Permit(MAction.GetVotesDB, MState.OnVotesDB);
+            
+            
+            // string graph = UmlDotGraph.Format(bot.GetInfo());
+            // Console.WriteLine("GPAPH||||||||||  \n" + graph + "\n||||||||||");
+            
+             bot.Fire(MAction.GetMainMenu);
+             Console.WriteLine($"CurrentState: {bot.State}\n");
+            
+             bot.FireAsync(MAction.GetAuth);
+             Console.WriteLine($"CurrentState: {bot.State}\n");
 
         }
 
