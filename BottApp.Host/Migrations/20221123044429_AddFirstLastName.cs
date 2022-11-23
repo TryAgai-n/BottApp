d24c2393ee9b2de6bb15f6a0b97df43e2935b830
User <gold.like.simple@gmail.com>
2022-11-23 15:09:13 +1000

    Add migration for User(add First And Last name)

---
+++ BottApp.Host/Migrations/20221123044429_AddFirstLastName.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BottApp.Host.Migrations
+{
+    public partial class AddFirstLastName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "User",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TelegramFirstName",
+                table: "User",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "TelegramFirstName",
+                table: "User");
+        }
+    }
+}
