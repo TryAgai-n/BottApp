1886fc58689c0fa066e58cc5873381d2865637b3
User <gold.like.simple@gmail.com>
2023-01-30 20:42:45 +1000

    ! create database fixture for tests fix

--- BottApp.Database.Test/Fixture.cs
+++ BottApp.Database.Test/Fixture.cs
@@ -16,20 +16,20 @@ internal sealed class Fixture : IDisposable
     }
 
 
-    public Fixture Create()
+    public static Fixture Create()
     {
         AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-    
+
         var guid = Guid.NewGuid().ToString("N");
         var option = new DbContextOptionsBuilder<PostgreSqlContext>()
             .UseNpgsql(
                 "User ID=postgres;Password=123;Host=localhost;Port=5432;Database=bottapp_test_" + guid + ";Pooling=true;",
-                 b => b.MigrationsAssembly(typeof(Startup).Assembly.GetName().Name)
+                b => b.MigrationsAssembly(typeof(Startup).Assembly.GetName().Name)
             )
             .Options;
-    
+
         var context = new PostgreSqlContext(option, new NullLoggerFactory());
-    
+
         context.Database.Migrate();
         
         return new Fixture(context);
